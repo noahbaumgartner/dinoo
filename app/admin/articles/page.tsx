b8408977ab25ae21933b9367b5e0c87fa7e4e15c
noah <baumgnoa@students.zhaw.ch>
2025-07-27 10:41:46 +0200

    started with article page

--- app/admin/articles/page.tsx
+++ app/admin/articles/page.tsx
@@ -6,6 +6,32 @@ export default function AdminArticlesPage() {
     return (
         <>
             <AdminHeader title="Artikel" />
+            <div className=" px-4 py-4 flex flex-col space-y-4">
+                <h1 className="text-2xl font-semibold px-2">Artikelverwaltung</h1>
+                <Tabs defaultValue="account" className="flex flex-col space-y-2">
+                    <TabsList>
+                        <TabsTrigger value="articles">Artikel</TabsTrigger>
+                        <TabsTrigger value="categories">Kategorien</TabsTrigger>
+                        <TabsTrigger value="times">Zeiten</TabsTrigger>
+                        <TabsTrigger value="outputs">Outputs</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="account">
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Card Title</CardTitle>
+                                <CardDescription>Card Description</CardDescription>
+                                <CardAction>Card Action</CardAction>
+                            </CardHeader>
+                            <CardContent>
+                                <p>Card Content</p>
+                            </CardContent>
+                            <CardFooter>
+                                <p>Card Footer</p>
+                            </CardFooter>
+                        </Card>
+                    </TabsContent>
+                </Tabs>
+            </div>
         </>
     );
 }
