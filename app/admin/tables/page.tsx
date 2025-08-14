97b4909207e207d56af7477414c79348a1dcf179
noah <baumgnoa@students.zhaw.ch>
2025-08-14 16:56:06 +0200

    began with table layouting

--- app/admin/tables/page.tsx
+++ app/admin/tables/page.tsx
@@ -1,9 +1,124 @@
 import { PageWrapper } from "@/components/page-wrapper";
+import { buttonVariants } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { size } from "zod";
 
 export default function AdminTablesPage() {
     return (
         <PageWrapper title="Tische">
-            <div></div>
+            <div className="flex flex-col space-y-2 h-96 w-full bg-sidebar border rounded-md p-2 overflow-auto">
+                <div className="flex flex-row space-x-2">
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                </div>
+                <div className="flex flex-row space-x-2">
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                </div>
+                <div className="flex flex-row space-x-2">
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                    <div className="flex flex-col border bg-background shadow-xs dark:bg-input/30 dark:border-input dark:hover:bg-input/50 rounded-md px-2 py-1.5 space-y-0.5 text-center" >
+                        <span className="text-lg px-1 font-semibold">101</span>
+                        <Separator />
+                        <span className="text-xs font-bold text-gray-300">deaktivieren</span>
+                    </div>
+                </div>
+            </div>
         </PageWrapper>
     );
 }
