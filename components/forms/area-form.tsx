3b207e1a7c705e5301ed3b6ba3f1ed02b689abc8
noah <baumgnoa@students.zhaw.ch>
2025-08-16 14:35:07 +0200

    Update area-form.tsx

--- components/forms/area-form.tsx
+++ components/forms/area-form.tsx
@@ -4,10 +4,13 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod";
+import { string, z } from "zod";
 import FormActions from "../form-actions";
 import type { Area } from "@/lib/prisma";
 import { createArea, deleteArea, updateArea } from "@/lib/actions/area";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { SquareMinus, SquarePlus } from "lucide-react";
 
 const formSchema = z.object({
     name: z.string().min(3, {
@@ -15,6 +18,7 @@ const formSchema = z.object({
     }).max(100, {
         message: "Der Name darf maximal 100 Zeichen lang sein.",
     }),
+    area: z.string(),
     index: z.number().min(0, {
         message: "Der Index muss mindestens 0 sein.",
     })
@@ -28,6 +32,7 @@ export default function AreaForm({ mode, area }: { mode: "create" | "edit"; area
             index: area?.index || 0,
         },
     })
+    const [areaState, setAreaState] = useState<string[][]>(JSON.parse("[[\"101\"]]"));
 
     return (
         <Form {...form}>
@@ -47,10 +52,53 @@ export default function AreaForm({ mode, area }: { mode: "create" | "edit"; area
                         </FormItem>
                     )}
                 />
+                <FormField
+                    control={form.control}
+                    name="area"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Bereich</FormLabel>
+                            <FormControl>
+                                <div className="flex flex-col">
+                                    <div className="flex flex-wrap space-y-2 space-x-2">
+                                        <Button variant="outline">
+                                            <SquarePlus className="size-4" />
+                                            Reihe
+                                        </Button>
+                                        <Button variant="outline">
+                                            <SquareMinus className="size-4" />
+                                            Reihe
+                                        </Button>
+                                        <Button variant="outline">
+                                            <SquarePlus className="size-4" />
+                                            Spalte
+                                        </Button>
+                                        <Button variant="outline">
+                                            <SquareMinus className="size-4" />
+                                            Spalte
+                                        </Button>
+                                    </div>
+                                    <div className="min-h-72 w-full bg-sidebar border p-2 rounded-md shadow-xs">
+                                        {areaState.map((row, rowIndex) => (
+                                            <div key={rowIndex}>
+                                                {row.map((cell, cellIndex) => (
+                                                    <div key={cellIndex}>
+                                                        <input type="text" />
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
                 <FormActions mode={mode} cancelUrl="/admin/areas" deleteAction={async () => {
                     deleteArea(area?.id || "");
                 }} />
             </form>
-        </Form>
+        </Form >
     );
 }
